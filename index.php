aaad4f38a4dd05bbf93e2926bf3f94a96fe07d51
mrGets <119437080+mrGets@users.noreply.github.com>
2022-11-29 21:36:19 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+
+
+<?php 
+ $p = 'Приветствую Вас на моей страничке!';
+?>
+
+<?php 
+ $name = 'Jake';
+ $surname = 'Gatsby';
+ $city = 'Los-Angeles';
+ $age = 30;
+?>
+
+
+<?php
+include 'main.php';
+?>
+
